31975bcce316cb2b6eb8a78085c3bada80ff0927
Jason Casey <j.casey@beamio.net>
2020-08-05 15:55:22 -0400

    removed out of date test

--- tk_builder/tests/canvas_tests.py
+++ tk_builder/tests/canvas_tests.py
@@ -34,27 +34,7 @@ class ImageCanvasTests(unittest.TestCase):
         print("one of the x or y dimensions of the display image matches the canvas")
         print("test passed")
 
-    # TODO: there are some conflicting things to take care of with how either the image canvas or canvas image object
-    # TODO: handle rescaling to fit the canvas or not.
-    def test_display_image_init_w_numpy_not_scaled_to_fit(self):
-        image_data = np.zeros((self.image_ny, self.image_nx))
-        numpy_reader = NumpyImageReader(image_data)
-        image_canvas = ImageCanvas(None)
-        image_canvas.set_canvas_size(self.canvas_nx, self.canvas_ny)
-        canvas_image = CanvasImage(numpy_reader, self.canvas_nx, self.canvas_ny)
-        image_canvas.variables.canvas_image_object = canvas_image
-        image_canvas.variables.canvas_image_object.scale_to_fit_canvas = False
-        image_canvas.variables.rescale_image_to_fit_canvas = False
-
-        display_image = image_canvas.variables.canvas_image_object.display_image
-        display_ny, display_nx = np.shape(display_image)
-        assert display_ny > image_canvas.variables.canvas_height or display_nx > image_canvas.variables.canvas_width
-        print("")
-        print("display image is larger or equal to the canvas size")
-        print("test passed")
-
-
-    #TODO: handle case for non integer values for canvas rect
+    # TODO: handle case for non integer values for canvas rect
     def test_image_in_rect_after_zoom(self):
         image_data = np.zeros((self.image_ny, self.image_nx))
         numpy_reader = NumpyImageReader(image_data)
