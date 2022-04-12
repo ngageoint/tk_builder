794f083fc478c1eba46ee044cb71e023da1be7a3
Thomas McCullough <thomas.mccullough@gmail.com>
2022-04-12 13:12:01 -0400

    pyplot handling modifications continued

--- tk_builder/widgets/pyplot_frame.py
+++ tk_builder/widgets/pyplot_frame.py
@@ -8,6 +8,7 @@ import tkinter
 
 import numpy
 
+from tk_builder.widgets.image_canvas import ImageCanvas
 from tk_builder.widgets.basic_widgets import Frame
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, \
@@ -65,7 +66,78 @@ class PyplotFrame(Frame):
         Frame.destroy(self)
 
 
-class PyplotImagePanel(PyplotFrame):
+class PyplotFigure(PyplotFrame):
+    """
+    Provides a widget that allows users to embed a simple one axis figure
+    in an application
+    """
+
+    def __init__(self, parent, navigation=False):
+        """
+
+        Parameters
+        ----------
+        parent
+        navigation : bool
+        """
+
+        self.x_label = ''
+        self.y_label = ''
+        self.title = ''
+        fig, self.ax = pyplot.subplots(dpi=100, nrows=1, ncols=1)
+        PyplotFrame.__init__(self, parent, fig, navigation=navigation)
+        self.clear()
+
+    def clear(self):
+        """
+        Clear the axes contents.
+        """
+
+        self.ax.cla()
+        self.ax.set_title(self.title)
+        self.ax.set_xlabel(self.x_label)
+        self.ax.set_ylabel(self.y_label)
+        self.ax.grid(True)
+        self.ax.set_axisbelow(True)
+        self.draw()
+
+    def set_xlabel(self, the_label):
+        """
+        Sets the displayed x label.
+
+        Parameters
+        ----------
+        the_label : str
+        """
+        self.x_label = the_label
+        self.ax.set_xlabel(the_label)
+
+    def set_ylabel(self, the_label):
+        """
+        Sets the displayed y label.
+
+        Parameters
+        ----------
+        the_label : str
+        """
+
+        self.y_label = the_label
+        self.ax.set_ylabel(the_label)
+
+    def set_title(self, the_title):
+        """
+        Sets the displayed figure title.
+
+        Parameters
+        ----------
+        the_title : str
+        """
+
+        self.title = the_title
+        self.ax.set_title(the_title)
+
+
+class PyplotImagePanel(PyplotFigure):
     """
     Provides a widget that allows users to embed pyplot images into an application.
     """
@@ -81,26 +153,12 @@ class PyplotImagePanel(PyplotFrame):
         """
 
         self._cmap_name = DEFAULT_CMAP
-        self.x_label = None
-        self.y_label = None
-        self.title = None
-        fig, self.ax = pyplot.subplots(dpi=100, nrows=1, ncols=1)
-        PyplotFrame.__init__(self, parent, fig, navigation=navigation)
-
+        PyplotFigure.__init__(self, parent, navigation=navigation)
         self.cmap_name = cmap_name
+        self.set_title('Detailed View')
+        self.set_xlabel('Column [pixel]')
+        self.set_ylabel('Row [pixel]')
         self.make_blank()
-        self.set_title('detailed view')
-        self.set_ylabel('row')
-        self.set_xlabel('column')
-        self.ax.grid(False)
-
-    def make_blank(self):
-        """
-        Clear the contents of the image panel, and make it blank placeholder.
-        """
-
-        image_data = numpy.zeros((600, 400), dtype='uint8')
-        self.update_image(image_data)
 
     @property
     def cmap_name(self):
@@ -119,6 +177,14 @@ class PyplotImagePanel(PyplotFrame):
             logger.error(
                 'cmap_name {} is not in the pyplot list of registered colormaps. '
                 'Using the default.'.format(value))
+
+    def make_blank(self):
+        """
+        Clear the contents of the image panel, and make it blank placeholder.
+        """
+
+        image_data = numpy.zeros((600, 400), dtype='uint8')
+        self.update_image(image_data)
 
     def clear(self):
         """
@@ -172,37 +238,137 @@ class PyplotImagePanel(PyplotFrame):
         self.ax.pcolormesh(x_array, y_array, image_data, **kwargs)
         self.draw()
 
-    def set_xlabel(self, the_label):
+
+class ImagePanelDetail(object):
+    """
+    Standard popup detail pane for image canvas.
+
+    .. warning::
+        This functionality requires binding to the <<SelectionChanged>>,
+        <<SelectionFinalized>>, and <<RemapChanged>> events of the image canvas.
+    """
+
+    def __init__(self,
+                 master,
+                 image_canvas,
+                 on_selection_changed=False,
+                 on_selection_finalized=True,
+                 fetch_full_resolution=False):
         """
-        Sets the displayed x label.
 
         Parameters
         ----------
-        the_label : str
+        master : tkinter.Tk|tkinter.Toplevel
+        image_canvas : ImageCanvas
+            The associated image canvas
+        on_selection_changed : bool
+            Trigger popup when selection has changed?
+        on_selection_finalized : bool
+            Trigger popup when selection has finalized?
+        fetch_full_resolution : bool
+            Fetch the selection data at full resolution?
         """
-        self.x_label = the_label
-        self.ax.set_xlabel(the_label)
 
-    def set_ylabel(self, the_label):
+        self.detail_popup = tkinter.Toplevel(master)
+        self.detail_popup.geometry('700x500')
+        self.pyplot_panel = PyplotImagePanel(self.detail_popup, navigation=True)  # type: PyplotImagePanel
+        self.pyplot_panel.set_title('Detail View')
+        self.detail_popup.protocol("WM_DELETE_WINDOW", self.detail_popup.withdraw)
+        self.detail_popup.withdraw()
+
+        self.image_canvas = image_canvas
+        self.on_selection_changed = on_selection_changed
+        self.on_selection_finalized = on_selection_finalized
+        self.fetch_full_resolution = fetch_full_resolution
+
+        self.image_canvas.bind('<<SelectionChanged>>', self.handle_detail_selection_change, '+')
+        self.image_canvas.bind('<<SelectionFinalized>>', self.handle_detail_selection_finalized, '+')
+        self.image_canvas.bind('<<RemapChanged>>', self.handle_detail_remap_change, '+')
+
+    def make_blank(self):
+        self.pyplot_panel.make_blank()
+
+    def set_focus_on_detail_popup(self):
+        self.detail_popup.deiconify()
+        self.detail_popup.focus_set()
+        self.detail_popup.lift()
+
+    def detail_popup_callback(self):
+        self.detail_popup.deiconify()
+
+    def display_canvas_rect_selection_in_pyplot_frame(self):
+        def get_extent(coords):
+            left = min(coords[1::2])
+            right = max(coords[1::2])
+            top = max(coords[0::2])
+            bottom = min(coords[0::2])
+            return left, right, top, bottom
+
+        threshold = self.image_canvas.variables.config.select_size_threshold
+
+        try:
+            select_id = self.image_canvas.variables.get_tool_shape_id_by_name('SELECT')
+            if select_id is None:
+                return
+        except KeyError:
+            return
+
+        rect_coords = self.image_canvas.get_shape_image_coords(select_id)
+        extent = get_extent(rect_coords)
+
+        if abs(extent[1] - extent[0]) < threshold or abs(extent[2] - extent[3]) < threshold:
+            self.pyplot_panel.make_blank()
+        else:
+            image_data = self.image_canvas.get_image_data_in_canvas_rect_by_id(
+                select_id, decimation=1 if self.fetch_full_resolution else None)
+            if image_data is not None:
+                self.pyplot_panel.update_image(image_data, extent=extent)
+            else:
+                self.pyplot_panel.make_blank()
+
+    # noinspection PyUnusedLocal
+    def handle_detail_selection_change(self, event):
         """
-        Sets the displayed y label.
+        Handle a change in the selection area.
 
         Parameters
         ----------
-        the_label : str
+        event
         """
 
-        self.y_label = the_label
-        self.ax.set_ylabel(the_label)
+        if self.image_canvas.image_reader is None or \
+                not self.on_selection_changed:
+            return
 
-    def set_title(self, the_title):
+        self.display_canvas_rect_selection_in_pyplot_frame()
+        self.detail_popup_callback()
+
+    # noinspection PyUnusedLocal
+    def handle_detail_selection_finalized(self, event):
         """
-        Sets the displayed figure title.
+        Handle the finalization of the selection area.
 
         Parameters
         ----------
-        the_title : str
+        event
         """
 
-        self.title = the_title
-        self.ax.set_title(the_title)
+        if self.image_canvas.image_reader is None or \
+                not self.on_selection_finalized:
+            return
+
+        self.display_canvas_rect_selection_in_pyplot_frame()
+        self.set_focus_on_detail_popup()
+
+    # noinspection PyUnusedLocal
+    def handle_detail_remap_change(self, event):
+        """
+        Handle that the remap for the image canvas has changed.
+
+        Parameters
+        ----------
+        event
+        """
+
+        if self.image_canvas.image_reader is not None:
+            self.display_canvas_rect_selection_in_pyplot_frame()
